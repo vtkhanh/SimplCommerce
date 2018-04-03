a71d23926112a77471ffb1f2f288567f78ae60e4
Khanh Vu <vtkhanh90@gmail.com>
2018-04-03 08:29:44 +0700

    Refactor gulp

--- src/SimplCommerce.WebHost/gulpfile.js
+++ src/SimplCommerce.WebHost/gulpfile.js
@@ -1,38 +1,35 @@
-﻿/// <binding AfterBuild='copy-modules' />
-"use strict";
+﻿"use strict";
 
-var gulp = require('gulp'),
-    clean = require('gulp-clean'),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify"),
-    ignore = require('gulp-ignore'),
-    glob = require("glob"),
-    rimraf = require("rimraf");
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const glob = require("glob");
+const rimraf = require("rimraf");
+const concat = require("gulp-concat");
+const cssmin = require("gulp-cssmin");
+const uglify = require("gulp-uglify");
+const ignore = require('gulp-ignore');
+const del = require('del');
+const argv = require('yargs').argv;
+const install = require('gulp-install');
 
-var paths = {
-    host: {
-        wwwroot: "./wwwroot/",
-        wwwrootModules: "./wwwroot/modules/",
-        modules: "./Modules/",
-        moduleBin: "/bin/",
-        bower: "./bower_components/"
-    },
-    dev: {
-        modules: "../Modules/",
-        moduleBin: "/bin/Debug/netcoreapp2.0/"
-    }
+
+const configurationName = argv.configurationName || 'Debug';
+const targetFramework = 'netcoreapp2.1';
+
+const mPaths = {
+    devModules: "../Modules/",
+    hostModules: "./Modules/",
+    hostWwwrootModules: "./wwwroot/modules/"
 };
 
-paths.host.js = paths.host.wwwroot + "modules/**/*.js";
-paths.host.minJs = paths.host.wwwroot + "modules/**/*.min.js";
-paths.host.css = paths.host.wwwroot + "modules/**/*.css";
-paths.host.minCss = paths.host.wwwroot + "modules/**/*.min.css";
-paths.host.concatJsDest = paths.host.wwwroot + "js/site.min.js";
-paths.host.concatCssDest = paths.host.wwwroot + "css/site.min.css";
-paths.host.lib = paths.host.wwwroot + "lib/";
+const modules = loadModules();
 
-var bower = {
+const paths = {
+    webroot: "./wwwroot/",
+    bower: "./bower_components/"
+};
+
+const bower = {
     "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,woff2,eot}",
     "font-awesome": "components-font-awesome/**/*.{css,ttf,svg,woff,woff2,eot,otf}",
     "jquery": "jquery/dist/jquery*.{js,map}",
@@ -60,99 +57,113 @@ var bower = {
     "bootstrap-ui-datetime-picker": "bootstrap-ui-datetime-picker/dist/*.{js,tpls.js}"
 };
 
-var modules = loadModules();
+gulp.task('clean-modules', function () {
+    return gulp.src([mPaths.hostModules + '*', mPaths.hostWwwrootModules + '*'], {
+        read: false
+    })
+    .pipe(clean());
+});
+
+gulp.task('copy-compiled', ['clean-modules'], function () {
+    modules.forEach(function (module) {
+        gulp.src(mPaths.devModules + module.fullName + `/bin/${configurationName}/${targetFramework}/**/*.*`)
+        .pipe(gulp.dest(mPaths.hostModules + module.fullName + '/bin'));
+    });
+});
+
+gulp.task('copy-static', ['clean-modules'], function () {
+    modules.forEach(function (module) {
+        gulp.src([mPaths.devModules + module.fullName + '/Views/**/*.*', mPaths.devModules + module.fullName + '/module.json'], {
+            base: module.fullName
+        })
+        .pipe(gulp.dest(mPaths.hostModules + module.fullName));
+        
+        gulp.src(mPaths.devModules + module.fullName + '/wwwroot/**/*.*')
+        .pipe(gulp.dest(mPaths.hostWwwrootModules + module.name));
+    });
+    
+    gulp.src(mPaths.devModules + 'SimplCommerce.Module.SampleData/SampleContent/**/*.*')
+    .pipe(gulp.dest(mPaths.hostModules + 'SimplCommerce.Module.SampleData/SampleContent'));
+});
+
+gulp.task('copy-modules', ['clean-modules', 'copy-static', 'copy-compiled']);
+
+gulp.task('watch', () => {
+    // watch([mPaths.devModules + '**/Views/**/*.*', mPaths.devModules + '**/wwwroot/**/*.*'], )
+});
 
 function loadModules() {
-    var moduleManifestPaths,
-        modules = [];
+    let moduleManifestPaths;
+    const modules = [];
 
-    moduleManifestPaths = glob.sync(paths.dev.modules + '*.*/module.json', {});
+    moduleManifestPaths = glob.sync(mPaths.devModules + 'SimplCommerce.Module.*/module.json', {});
     moduleManifestPaths.forEach(function (moduleManifestPath) {
-        // console.log(moduleManifestPath);
-        var moduleManifest = require(moduleManifestPath);
+        const moduleManifest = require(moduleManifestPath);
         modules.push(moduleManifest);
     });
 
     return modules;
 }
 
-gulp.task('clean-modules', function () {
-    return gulp.src([paths.host.modules + '*', paths.host.wwwrootModules + '*'], {
-            read: false
-        })
-        .pipe(clean());
-});
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.lib = paths.webroot + "lib/";
 
-gulp.task('copy-static', function () {
-    modules.forEach(function (module) {
-        gulp.src([paths.dev.modules + module.fullName + '/Views/**/*.*',
-                paths.dev.modules + module.fullName + '/module.json'
-            ], {
-                base: module.fullName
-            })
-            .pipe(gulp.dest(paths.host.modules + module.fullName));
 
-        gulp.src(paths.dev.modules + module.fullName + '/wwwroot/**/*.*')
-            .pipe(gulp.dest(paths.host.wwwrootModules + module.name));
-    });
-
-    gulp.src(paths.dev.modules + 'SimplCommerce.Module.SampleData/SampleContent/**/*.*')
-        .pipe(gulp.dest(paths.host.modules + 'SimplCommerce.Module.SampleData/SampleContent'));
-});
-
-gulp.task('copy-modules', ['clean-modules'], function () {
-    gulp.start(['copy-static']);
-
-    modules.forEach(function (module) {
-        gulp.src(paths.dev.modules + module.fullName + paths.dev.moduleBin + '**/*.*')
-            .pipe(gulp.dest(paths.host.modules + module.fullName + paths.host.moduleBin));
-    });
+// Install packages
+gulp.task('install:bower', () => {
+    return gulp.src(['./bower.json'])
+        .pipe(install());
 });
 
 gulp.task("clean:js", function (cb) {
-    rimraf(paths.host.concatJsDest, cb);
+    rimraf(paths.concatJsDest, cb);
 });
 
 gulp.task("clean:css", function (cb) {
-    rimraf(paths.host.concatCssDest, cb);
+    rimraf(paths.concatCssDest, cb);
 });
 
 gulp.task("clean:lib", function () {
-    for (var desDir in bower)
-        rimraf.sync(paths.host.lib + desDir);
+    for (let desDir in bower) {
+        del.sync(paths.lib + desDir);
+    }
 });
 
-gulp.task("copy-lib", ["clean:lib"], function () {
-    var ignoreComponents = ["**/npm.js"];
+gulp.task("copy-lib", ["install:bower", "clean:lib"], function () {
+    const ignoreComponents = ["**/npm.js"];
 
-    for (var desDir in bower) {
-        // console.log(paths.host.bower + bower[desDir]);
-        gulp.src(paths.host.bower + bower[desDir])
+    for (let desDir in bower) {
+        gulp.src(paths.bower + bower[desDir])
             .pipe(ignore.exclude(ignoreComponents))
-            .pipe(gulp.dest(paths.host.lib + desDir));
+            .pipe(gulp.dest(paths.lib + desDir));
     }
 });
 
 gulp.task("min:js", function () {
-    return gulp.src([paths.host.js, "!" + paths.host.minJs], {
+    return gulp.src([paths.js, "!" + paths.minJs], {
             base: "."
         })
-        .pipe(concat(paths.host.concatJsDest))
-        // .pipe(uglify())
+        .pipe(concat(paths.concatJsDest))
         .pipe(gulp.dest("."));
 });
 
 gulp.task("min:css", function () {
-    return gulp.src([paths.host.css, "!" + paths.host.minCss])
-        .pipe(concat(paths.host.concatCssDest))
+    return gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
         .pipe(cssmin())
         .pipe(gulp.dest("."));
 });
 
 gulp.task("min", ["min:js", "min:css"]);
 
-// gulp.task('watch', function () {
-//     gulp.watch([paths.dev.modules + '**/*.js', paths.dev.modules + '**/*.css', paths.dev.modules + '**/*.cshtml', paths.dev.modules + '**/*.json'], ['copy-static']);
-// });
+gulp.task('watch', () => {
+    gulp.watch([mPaths.devModules + '**/Views/**/*.*', mPaths.devModules + '**/wwwroot/**/*.*'], ['copy-static']);
+});
 
-gulp.task('default', ['copy-lib', 'copy-modules']);
+gulp.task('default', ['copy-lib', 'copy-modules'], () => {
+});
