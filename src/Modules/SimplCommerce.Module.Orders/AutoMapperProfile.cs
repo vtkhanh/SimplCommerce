b90cb226ba38bde51e308aa368ba057aeabc62a8
Khanh Vu <vtkhanh90@gmail.com>
2018-04-26 16:08:54 +0700

    Include AutoMapper in Order module; Use md-datepicker for st-date-range

---
+++ src/Modules/SimplCommerce.Module.Orders/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Newtonsoft.Json;
+using SimplCommerce.Module.Catalog.Models;
+using SimplCommerce.Module.Catalog.ViewModels;
+
+namespace SimplCommerce.Module.Orders
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+        }
+    }
+}
