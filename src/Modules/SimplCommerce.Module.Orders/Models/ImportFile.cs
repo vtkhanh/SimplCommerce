d0a11ad88266e2d0cebd40a60b63cfaf3b3c686a
Khanh Vu <vtkhanh90@gmail.com>
2019-04-24 07:04:27 +0100

    Create models

---
+++ src/Modules/SimplCommerce.Module.Orders/Models/ImportFile.cs
@@ -0,0 +1,16 @@
+﻿using SimplCommerce.Infrastructure.Models;
+using SimplCommerce.Module.Orders.Models.Enums;
+
+namespace SimplCommerce.Module.Orders.Models
+{
+    public class ImportFile : EntityBase
+    {
+        public string FileName { get; set; }
+
+        public string UID { get; set; }
+
+        public ImportFileStatus Status { get; set; }
+
+        public int RunCount { get; set; }
+    }
+}
---
+++ src/Modules/SimplCommerce.Module.Orders/Models/ImportResult.cs
@@ -0,0 +1,13 @@
+﻿using SimplCommerce.Infrastructure.Models;
+
+namespace SimplCommerce.Module.Orders.Models
+{
+    public class ImportResult : EntityBase
+    {
+        public long ImportFileId { get; set; }
+
+        public int SuccessCount { get; set; }
+
+        public int FailureCount { get; set; }
+    }
+}
