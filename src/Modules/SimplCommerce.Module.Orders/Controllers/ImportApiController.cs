146a7d6b1c90700085a7b54ba950d87baf51d185
Khanh Vu <vtkhanh90@gmail.com>
2019-04-24 07:04:27 +0100

    Create models

---
+++ src/Modules/SimplCommerce.Module.Orders/Controllers/ImportApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimplCommerce.Module.Core.Extensions.Constants;
+
+namespace SimplCommerce.Module.Orders.Controllers
+{
+    [Authorize(Policy = Policy.CanManageOrder)]
+    [Route("api/orders")]
+    [ApiController]
+    public class ImportApiController : Controller
+    {
+        
+    }
+}
---
+++ src/Modules/SimplCommerce.Module.Orders/Models/ImportResultDetail.cs
@@ -0,0 +1,16 @@
+﻿using SimplCommerce.Infrastructure.Models;
+using SimplCommerce.Module.Orders.Models.Enums;
+
+namespace SimplCommerce.Module.Orders.Models
+{
+    public class ImportResultDetail : EntityBase
+    {
+        public long ImportResultId { get; set; }
+
+        public int LineNumber { get; set; }
+
+        public ImportResultDetailStatus Status { get; set; }
+
+        public string Message { get; set; }
+    }
+}
