c166e5d91284dace55996c79c313971966cda61c
Khanh Vu <vtkhanh90@gmail.com>
2019-05-22 23:55:51 +0100

    Get list of order import files

--- src/Modules/SimplCommerce.Module.Orders/Services/IOrderFileService.cs
+++ src/Modules/SimplCommerce.Module.Orders/Services/IOrderFileService.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using SimplCommerce.Infrastructure.Web.SmartTable;
 using SimplCommerce.Module.Orders.Services.Dtos;
 
 namespace SimplCommerce.Module.Orders.Services
@@ -7,6 +7,6 @@ namespace SimplCommerce.Module.Orders.Services
     public interface IOrderFileService
     {
         Task SaveAsync(SaveOrderFileDto request);
-        Task<IList<GetOrderFileDto>> GetAsync();
+        SmartTableResult<GetOrderFileDto> GetOrderFiles(SmartTableParam param);
     }
 }
