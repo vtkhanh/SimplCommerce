5b448186b00a3cb8c1ca53558ff1d96ff61f42dd
Khanh Vu <vtkhanh90@gmail.com>
2019-06-05 23:05:55 +0100

    Add request/handler with mediator pattern

--- src/Modules/SimplCommerce.Module.Orders/Services/IOrderFileService.cs
+++ src/Modules/SimplCommerce.Module.Orders/Services/IOrderFileService.cs
@@ -1,12 +1,15 @@
 ﻿using System.Threading.Tasks;
 using SimplCommerce.Infrastructure.Web.SmartTable;
+using SimplCommerce.Module.Orders.Models.Enums;
 using SimplCommerce.Module.Orders.Services.Dtos;
 
 namespace SimplCommerce.Module.Orders.Services
 {
     public interface IOrderFileService
     {
-        Task SaveAsync(SaveOrderFileDto request);
-        SmartTableResult<GetOrderFileDto> GetOrderFiles(SmartTableParam param);
+        Task<GetOrderFileDto> GetByIdAsync(long id);
+        SmartTableResult<GetOrderFileDto> Get(SmartTableParam param);
+        Task<long> SaveAsync(SaveOrderFileDto request);
+        Task<bool> UpdateStatusAsync(long id, ImportFileStatus status);
     }
 }
