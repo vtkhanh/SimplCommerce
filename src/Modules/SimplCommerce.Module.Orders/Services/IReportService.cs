2c96aeb69b0230c2f35cfebd5640d9c60bea658a
Khanh Vu <vtkhanh90@gmail.com>
2019-06-12 23:37:04 +0100

    Fix reports which missing orders in a month

--- src/Modules/SimplCommerce.Module.Orders/Services/IReportService.cs
+++ src/Modules/SimplCommerce.Module.Orders/Services/IReportService.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
-using SimplCommerce.Module.Orders.Services.Dtos;
 
 namespace SimplCommerce.Module.Orders.Services
 {
     public interface IReportService
     {
-        Task<RevenueReportDto> GetRevenueReportAsync(DateTime time, long? createdById, int monthOffset = 3);
-        Task<RevenueReportDto> GetRevenueReportBySellerAsync(DateTime time, long sellerId, int monthOffset = 3);
+        Task<RevenueReportBuilder> GetRevenueReportAsync(DateTime time, long? createdById, int monthOffset = 3);
+        Task<RevenueReportBuilder> GetRevenueReportBySellerAsync(DateTime time, long sellerId, int monthOffset = 3);
     }
 }
---
+++ src/Modules/SimplCommerce.Module.Orders/Services/RevenueReportBuilder.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimplCommerce.Module.Orders.Models;
+
+namespace SimplCommerce.Module.Orders.Services
+{
+    public class RevenueReportBuilder
+    {
+        private const string MonthFormat = "MM/yyyy";
+
+        private readonly IReadOnlyCollection<Order> _orders;
+        private readonly DateTimeOffset _from;
+        private readonly DateTimeOffset _to;
+        private readonly int _diffMonths;
+
+        public RevenueReportBuilder(IReadOnlyCollection<Order> orders)
+        {
+            _orders = orders.OrderBy(item => item.CompletedOn).ToList();
+            _from = _orders.First().CompletedOn.Value;
+            _to = _orders.Last().CompletedOn.Value;
+            _diffMonths = (_to.Month + _to.Year * 12) - (_from.Month + _from.Year * 12);
+        }
+
+        public IList<decimal> SubTotals { get; } = new List<decimal>();
+        public IList<decimal> Totals { get; } = new List<decimal>();
+        public IList<decimal> Costs { get; } = new List<decimal>();
+        public IList<decimal> Profits { get; } = new List<decimal>();
+        public IList<string> Months { get; } = new List<string>();
+
+        public void EvaluateSubTotals()
+        {
+            Months.Clear();
+            Totals.Clear();
+
+            for (var index = 0; index <= _diffMonths; index++)
+            {
+                var period = _from.AddMonths(index);
+                var ordersInMonth = _orders.Where(IsInMonth(period.Month));
+
+                if (!ordersInMonth.Any())
+                    continue;
+
+                Months.Add(period.ToString(MonthFormat));
+                SubTotals.Add(ordersInMonth.Sum(order => order.SubTotal));
+            }
+        }
+
+        public void EvaluateTotals()
+        {
+            if (!SubTotals.Any()) // SubTotals need to be evaluated beforehand
+                EvaluateSubTotals();
+
+            Totals.Clear();
+
+            for (var index = 0; index <= _diffMonths; index++)
+            {
+                var period = _from.AddMonths(index);
+                var ordersInMonth = _orders.Where(IsInMonth(period.Month));
+
+                if (!ordersInMonth.Any())
+                    continue;
+
+                Totals.Add(ordersInMonth.Sum(order => order.OrderTotal));
+            }
+        }
+
+        public void EvaluateCostsAndProfits()
+        {
+            if (!Totals.Any()) // Totals need to be evaluated beforehand
+                EvaluateTotals();
+
+            Costs.Clear();
+            Profits.Clear();
+
+            for (var index = 0; index <= _diffMonths; index++)
+            {
+                var period = _from.AddMonths(index);
+                var ordersInMonth = _orders.Where(IsInMonth(period.Month));
+
+                if (!ordersInMonth.Any())
+                    continue;
+
+                Costs.Add(ordersInMonth.Sum(order => order.OrderTotalCost));
+                Profits.Add(Totals[index] - Costs[index]);
+            }
+        }
+
+        private static Func<Order, bool> IsInMonth(int month) => 
+            order => order.CompletedOn.HasValue && order.CompletedOn.Value.Month == month;
+    }
+}
