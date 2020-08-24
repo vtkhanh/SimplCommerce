06b1d27771cd25b6094f18407cc64718e9461c90
Khanh Vu <vtkhanh90@gmail.com>
2020-08-25 00:47:33 +0100

    Fix IAsyncQueryProvider.ExecuteAsync

--- test/SimplCommerce.Test.Shared/MockQueryable/TestAsyncEnumerator.cs
+++ test/SimplCommerce.Test.Shared/MockQueryable/TestAsyncEnumerator.cs
@@ -1,38 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SimplCommerce.Test.Shared.MockQueryable
+namespace SimplCommerce.Module.Core.Tests
 {
-    public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
     {
-        private readonly IEnumerator<T> _enumerator;
+        private readonly IEnumerator<T> _inner;
 
-        public TestAsyncEnumerator(IEnumerator<T> enumerator)
+        public TestAsyncEnumerator(IEnumerator<T> inner) => _inner = inner;
+
+        public T Current => _inner.Current;
+
+        public ValueTask<bool> MoveNextAsync()
         {
-            _enumerator = enumerator ?? throw new ArgumentNullException();
-        }
-
-        public T Current => _enumerator.Current;
-
-        public void Dispose()
-        {
+            return new ValueTask<bool>(_inner.MoveNext());
         }
 
         public ValueTask DisposeAsync()
         {
+            _inner.Dispose();
             return new ValueTask();
-        }
-
-        public Task<bool> MoveNext(CancellationToken cancellationToken)
-        {
-            return Task.FromResult(_enumerator.MoveNext());
-        }
-
-        public ValueTask<bool> MoveNextAsync()
-        {
-            return new ValueTask<bool>(_enumerator.MoveNext());
         }
     }
 }
--- test/SimplCommerce.Test.Shared/MockQueryable/TestExpressionVisitor.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Linq.Expressions;
-
-namespace SimplCommerce.Test.Shared.MockQueryable
-{
-    public class TestExpressionVisitor : ExpressionVisitor
-    {
-    }
-}
