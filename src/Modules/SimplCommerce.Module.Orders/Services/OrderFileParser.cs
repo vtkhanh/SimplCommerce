831c54c5c620ca4c5a65c37ba3dd73975b3dc97d
Khanh Vu <vtkhanh90@gmail.com>
2020-09-10 00:45:34 +0100

    Use Partial tag helper & ExecuteSqlRaw

--- src/Modules/SimplCommerce.Module.Orders/Services/OrderFileParser.cs
+++ src/Modules/SimplCommerce.Module.Orders/Services/OrderFileParser.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
-// using System.Linq;
 using ExcelDataReader;
 using Microsoft.EntityFrameworkCore.Internal;
 using SimplCommerce.Infrastructure;
